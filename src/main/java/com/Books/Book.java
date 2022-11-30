1724af17d879b02cf48d4cc435e13d8c24a95804
Diana-nyamai <dnyamai.dn@gmail.com>
2022-11-30 17:47:10 +0300

    Book data

---
+++ src/main/java/com/Books/Book.java
@@ -0,0 +1,20 @@
+package com.Books;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private String genre;
+    private String description;
+    private String isbn;
+    private String image;
+    private String published;
+    private String publisher;
+}
