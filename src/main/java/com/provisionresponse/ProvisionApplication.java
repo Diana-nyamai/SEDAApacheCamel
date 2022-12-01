f826589fe4a7b2eb68a4e89fb9970441437ab187
Diana-nyamai <dnyamai.dn@gmail.com>
2022-12-01 08:57:01 +0300

    ProvisionApplication

---
+++ src/main/java/com/provisionresponse/ProvisionApplication.java
@@ -0,0 +1,11 @@
+package com.provisionresponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvisionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProvisionApplication.class);
+    }
+}
