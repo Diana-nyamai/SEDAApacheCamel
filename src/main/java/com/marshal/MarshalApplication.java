a8deaa8dff7c2017dfa3c307e666416a08ff922d
Diana-nyamai <dnyamai.dn@gmail.com>
2022-12-01 08:56:26 +0300

    MarshalApplication

---
+++ src/main/java/com/marshal/MarshalApplication.java
@@ -0,0 +1,11 @@
+package com.marshal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarshalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MarshalApplication.class, args);
+    }
+}
