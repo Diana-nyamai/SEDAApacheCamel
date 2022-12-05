0b38c5f1ab6d1002d74a9d9855cdecce9182554b
Diana-nyamai <dnyamai.dn@gmail.com>
2022-12-05 14:36:38 +0300

    decisionapplication

---
+++ src/main/java/com/decisionmaking/DescisionApplication.java
@@ -0,0 +1,12 @@
+package com.decisionmaking;
+
+import org.apache.http.util.Args;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DescisionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DescisionApplication.class, args);
+    }
+}
